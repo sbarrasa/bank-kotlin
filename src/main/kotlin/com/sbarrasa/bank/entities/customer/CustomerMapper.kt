7e081aca23014609e0892ffe28cf3e1932003d69
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-05-27 02:51:03 -0300

    CustomerMapper

--- src/main/kotlin/com/sbarrasa/bank/entities/customer/CustomerMapper.kt
+++ src/main/kotlin/com/sbarrasa/bank/entities/customer/CustomerMapper.kt
@@ -2,6 +2,6 @@ package com.sbarrasa.bank.entities.customer
 
 import com.sbarrasa.util.ObjectMapper
 
-class CustomerMapper: ObjectMapper<Customer, Customer>({
+object CustomerMapper: ObjectMapper<Customer, Customer>({
     bindAll(Customer::class, Customer::class)
 })
