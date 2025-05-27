5dccd9cc39db81269675bdc6b4670ddd15004a9f
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-05-27 02:51:45 -0300

    Api update

--- src/main/kotlin/com/sbarrasa/bank/Aplication.kt
+++ src/main/kotlin/com/sbarrasa/bank/Aplication.kt
@@ -1,13 +1,17 @@
 package com.sbarrasa.bank
 
-import com.sbarrasa.bank.controller.CustomerRoutes
-import com.sbarrasa.bank.repository.MemCustomerRepository
+import com.sbarrasa.bank.config.CustomerModule
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+
 
 fun main(){
     embeddedServer(
@@ -18,11 +22,25 @@ fun main(){
     ).start(wait = true)
 }
 
+
 fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+
+    install(StatusPages) {
+        handleException<BadRequestException>(HttpStatusCode.BadRequest)
+        handleException<NotFoundException>(HttpStatusCode.NotFound)
+    }
+
     routing {
-        CustomerRoutes.register(this, MemCustomerRepository())
+        CustomerModule.register(this)
     }
 }
+
+inline fun <reified T : Throwable> StatusPagesConfig.handleException(status: HttpStatusCode) {
+    exception<T> { call, cause ->
+        call.respond(status, cause.message ?: status.description)
+    }
+}
+
