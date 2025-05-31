dcc450f7a0fe9f6498b193509edd0d160eb92ecd
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-05-31 18:32:11 -0300

    CaseUtils

---
+++ src/main/kotlin/com/sbarrasa/util/CaseUtils.kt
@@ -0,0 +1,36 @@
+package com.sbarrasa.util
+
+import kotlin.reflect.KClass
+
+
+enum class CaseType {
+    LOWER, UPPER, CAMEL, SNAKE, PASCAL
+}
+fun String.toCase(caseType: CaseType?): String = when(caseType) {
+    CaseType.LOWER -> lowercase()
+    CaseType.UPPER -> uppercase()
+    CaseType.CAMEL -> toCamelCase()
+    CaseType.SNAKE -> toSnakeCase()
+    CaseType.PASCAL -> toPascalCase()
+    else -> this
+}
+fun String.toCamelCase(): String =
+    split('_')
+        .mapIndexed { i, part ->
+            if (i == 0) part.lowercase()
+            else part.replaceFirstChar { it.uppercaseChar() }
+        }
+        .joinToString("")
+
+fun String.toPascalCase(): String =
+    split('_')
+        .joinToString("") { it.replaceFirstChar { c -> c.uppercaseChar() } }
+
+fun String.toSnakeCase(): String =
+    replace(Regex("([a-z])([A-Z])"), "$1_$2")
+        .replace(Regex("([A-Z])([A-Z][a-z])"), "$1_$2")
+        .lowercase()
+
+fun KClass<*>.simpleName(caseType: CaseType?): String =
+    simpleName!!.toCase(caseType)
+
