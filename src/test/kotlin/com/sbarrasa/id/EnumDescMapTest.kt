89e3c17e7c6a5f4761d6c381c85f225061cf8048
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-11-10 20:47:23 -0300

    enum utils catalog

---
+++ src/test/kotlin/com/sbarrasa/id/EnumDescMapTest.kt
@@ -0,0 +1,40 @@
+package com.sbarrasa.id
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class EnumDescMapTest {
+   enum class EnumTest(override val description: String) : Desc {
+      T1("test1"),
+      T2("test2"),
+      T3("test3");
+   }
+
+   val enumTestMap = EnumDescMappeable(EnumTest::class)
+
+   @Test
+   fun testEnumDescMapValues() {
+      val key = EnumTest.T1
+      assertEquals(
+         key.description, enumTestMap.asMap().get(key)
+      )
+   }
+
+   @Test
+   fun testEnumDescMapSize() {
+      assertEquals(EnumTest.entries.size, enumTestMap.asMap().size)
+   }
+
+   @Test
+   fun testEnumDescMapKeys() {
+      assertEquals(EnumTest.entries.toSet(), enumTestMap.asMap().keys)
+   }
+
+}
+
+
+
+
+
+
